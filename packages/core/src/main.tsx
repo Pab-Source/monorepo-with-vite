a0b029fa51462c78abefb6d4ff0b131038998171
Pablo Tirado <pablo.tirado.toledo@gmail.com>
2021-11-04 02:10:01 -0300

    add scripts to build

--- packages/core/src/main.tsx
+++ packages/core/src/main.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Core } from "./Core";
 
-const rootCore = Boolean(import.meta.env.VITE_APP_CORE);
+const core = Boolean(import.meta.env.VITE_APP_CORE);
 
-rootCore
-  ? ReactDOM.render(
-      <React.StrictMode>
-        <Core />
-      </React.StrictMode>,
-      document.getElementById("root")
-    )
-  : null;
+core &&
+  ReactDOM.render(
+    <React.StrictMode>
+      <Core />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 
 export { Core };
